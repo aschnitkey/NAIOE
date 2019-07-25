230fb916dac957c2155ec32df462e47fda4f82ce
aschnitkey <aschnitkey@gmail.com>
2019-07-25 15:16:32 -0400

    added video to market report section

---
+++ js/script-video.js
@@ -0,0 +1,9 @@
+// video selector
+
+let videoSource = ["marketing/marketreports/2019/nai_ohio_equities_2019q2_office.mp4", "marketing/marketreports/2019/nai_ohio_equities_2019q2_industrial.mp4"];
+
+let videoTag = document.getElementById("videoSourceTag");
+
+let randomNumber = Math.floor(Math.random() * videoSource.length);
+
+videoTag.setAttribute("src", videoSource[randomNumber]);
