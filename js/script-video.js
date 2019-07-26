d60a049ac8d9bfb45dfd55cf9b7ea2dc009170d9
aschnitkey <aschnitkey@gmail.com>
2019-07-26 09:59:47 -0400

    Added retail market report video to rotation

--- js/script-video.js
+++ js/script-video.js
@@ -1,6 +1,6 @@
 // video selector
 
-let videoSource = ["marketing/marketreports/2019/nai_ohio_equities_2019q2_office.mp4", "marketing/marketreports/2019/nai_ohio_equities_2019q2_industrial.mp4"];
+let videoSource = ["marketing/marketreports/2019/nai_ohio_equities_2019q2_office.mp4", "marketing/marketreports/2019/nai_ohio_equities_2019q2_industrial.mp4", "marketing/marketreports/2019/nai_ohio_equities_2019q2_retail.mp4"];
 
 let videoTag = document.getElementById("videoSourceTag");
 
