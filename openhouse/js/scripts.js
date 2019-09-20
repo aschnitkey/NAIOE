cea1eefbef6871664d9092e2c9140190d3ff112c
aschnitkey <aschnitkey@gmail.com>
2019-09-20 11:04:14 -0400

    Added parking map to Open House

--- openhouse/js/scripts.js
+++ openhouse/js/scripts.js
@@ -1,7 +1,9 @@
 let imageMain = document.querySelector(".lounge");
+let imageParking = document.querySelectorAll(".lounge")[1];
 
 function loadImage() {
     imageMain.style.opacity = 1;
+    imageParking.style.opacity = 1;
 }
 
 window.onload = loadImage;
