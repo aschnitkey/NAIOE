0c2ee8cc65af01c293c519d96d8b77be2c4342f0
aschnitkey <aschnitkey@gmail.com>
2019-10-15 14:33:54 -0400

    WAS code cleanup

--- js/script.js
+++ js/script.js
@@ -14,11 +14,3 @@ $('.ftr-image02').contenthover({
 // 	('.contents-hover').hide();
 // });
 
-function handleFirstTab(e) {
-	if (e.keyCode === 9) {
-		document.body.classList.add('user-is-tabbing');
-		window.removeEventListener('keydown', handleFirstTab);
-	}
-}
-
-window.addEventListener('keydown', handleFirstTab);
