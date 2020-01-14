c57d373ca53a50dc651ffa7b7c178361dfff69c8
aschnitkey <aschnitkey@gmail.com>
2020-01-14 11:15:24 -0500

    4th Quarter Market Report

--- js/script-video.js
+++ js/script-video.js
@@ -1,6 +1,6 @@
 // video selector
 
-let videoSource = ["marketing/marketreports/2019/nai_ohio_equities_2019q3_office.mp4", "marketing/marketreports/2019/nai_ohio_equities_2019q3_industrial.mp4", "marketing/marketreports/2019/nai_ohio_equities_2019q3_retail.mp4"];
+let videoSource = ["marketing/marketreports/2019/nai_ohio_equities_2019q4_office.mp4", "marketing/marketreports/2019/nai_ohio_equities_2019q4_industrial.mp4", "marketing/marketreports/2019/nai_ohio_equities_2019q4_retail.mp4"];
 
 let videoTag = document.getElementById("videoSourceTag");
 
