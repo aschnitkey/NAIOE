61fcc0cafb4417731299bdd60d870a126e92d2d3
aschnitkey <aschnitkey@gmail.com>
2020-01-20 10:15:52 -0500

    Updated Robbin C bio

---
+++ js/nameShorten.js
@@ -0,0 +1,3 @@
+// Add a class to shorten the line length for names in Bios that extend behind bio picture
+let nameTag = document.querySelector(".broker-bio-contact h3");
+nameTag.classList.add("nameShorten");
