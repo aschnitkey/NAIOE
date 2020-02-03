c280e76f1d3af9af962303d7ce88747e0973d153
aschnitkey <aschnitkey@gmail.com>
2020-02-03 16:58:31 -0500

    Remove Open House page

--- openhouse/js/scripts.js
+++
@@ -1,9 +0,0 @@
-let imageMain = document.querySelector(".lounge");
-let imageParking = document.querySelectorAll(".lounge")[1];
-
-function loadImage() {
-    imageMain.style.opacity = 1;
-    imageParking.style.opacity = 1;
-}
-
-window.onload = loadImage;
