a42689a34f3268bce2efb56d6efcc79461e0c0c8
Andrew Schnitkey <aschnitkey@gmail.com>
2020-04-10 13:57:40 -0400

    Added 2020q1 Market Reports

--- js/script-video.js
+++ js/script-video.js
@@ -1,6 +1,6 @@
 // video selector
 
-let videoSource = ["marketing/marketreports/2019/nai_ohio_equities_2019q4_office.mp4", "marketing/marketreports/2019/nai_ohio_equities_2019q4_industrial.mp4", "marketing/marketreports/2019/nai_ohio_equities_2019q4_retail.mp4"];
+let videoSource = ["marketing/marketreports/2020/nai_ohio_equities_2020q1_office.mp4", "marketing/marketreports/2020/nai_ohio_equities_2020q1_industrial.mp4", "marketing/marketreports/2020/nai_ohio_equities_2020q1_retail.mp4"];
 
 let videoTag = document.getElementById("videoSourceTag");
 
