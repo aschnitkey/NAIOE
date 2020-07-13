7c35a586e6a8b38fa86f9808f4b8fc59896ee7fd
aschnitkey <aschnitkey@gmail.com>
2020-07-13 11:11:09 -0400

    Updated market reports for Q2

--- js/script-video.js
+++ js/script-video.js
@@ -1,6 +1,6 @@
 // video selector
 
-let videoSource = ["marketing/marketreports/2020/nai_ohio_equities_2020q1_office.mp4", "marketing/marketreports/2020/nai_ohio_equities_2020q1_industrial.mp4", "marketing/marketreports/2020/nai_ohio_equities_2020q1_retail.mp4"];
+let videoSource = ["marketing/marketreports/2020/nai_ohio_equities_2020q2_office.mp4", "marketing/marketreports/2020/nai_ohio_equities_2020q2_industrial.mp4", "marketing/marketreports/2020/nai_ohio_equities_2020q2_retail.mp4"];
 
 let videoTag = document.getElementById("videoSourceTag");
 
