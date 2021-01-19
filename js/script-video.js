dda0494ae974f1f5be5ddf21d9f8597ab3544ddf
Andrew <aschnitkey@gmail.com>
2021-01-19 14:37:54 -0500

    Updated Market Reports for Q42020

--- js/script-video.js
+++ js/script-video.js
@@ -1,10 +1,10 @@
 // video selector
 
 let videoSource = [
-    "marketing/marketreports/2020/nai_ohio_equities_2020q3_office.mp4",
-    "marketing/marketreports/2020/nai_ohio_equities_2020q3_industrial.mp4",
-    "marketing/marketreports/2020/nai_ohio_equities_2020q3_retail.mp4",
-    "marketing/marketreports/2020/nai_ohio_equities_2020q3_multifamily.mp4"
+    "marketing/marketreports/2020/nai_ohio_equities_2020q4_office.mp4",
+    "marketing/marketreports/2020/nai_ohio_equities_2020q4_industrial.mp4",
+    "marketing/marketreports/2020/nai_ohio_equities_2020q4_retail.mp4",
+    "marketing/marketreports/2020/nai_ohio_equities_2020q4_multifamily.mp4"
 ];
 
 let videoTag = document.getElementById("videoSourceTag");
