8d298646aa9d84b44ac49c4e36c32423de01fd07
Andrew <aschnitkey@gmail.com>
2022-01-18 10:55:20 -0500

    add market reports for 2021q4

--- js/script-video.js
+++ js/script-video.js
@@ -1,14 +1,14 @@
 // video selector
 
 let videoSource = [
-    "marketing/marketreports/2021/nai_ohio_equities_2021q3_office.mp4",
-    "marketing/marketreports/2021/nai_ohio_equities_2021q3_industrial.mp4",
-    "marketing/marketreports/2021/nai_ohio_equities_2021q3_retail.mp4",
-    "marketing/marketreports/2021/nai_ohio_equities_2021q3_multifamily.mp4"
+  'marketing/marketreports/2021/nai_ohio_equities_2021q4_office.mp4',
+  'marketing/marketreports/2021/nai_ohio_equities_2021q4_industrial.mp4',
+  'marketing/marketreports/2021/nai_ohio_equities_2021q4_retail.mp4',
+  'marketing/marketreports/2021/nai_ohio_equities_2021q4_multifamily.mp4',
 ];
 
-let videoTag = document.getElementById("videoSourceTag");
+let videoTag = document.getElementById('videoSourceTag');
 
 let randomNumber = Math.floor(Math.random() * videoSource.length);
 
-videoTag.setAttribute("src", videoSource[randomNumber]);
+videoTag.setAttribute('src', videoSource[randomNumber]);
