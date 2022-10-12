649d7c7d605d8d30157a050588da8d0aa4642f06
Andrew <aschnitkey@gmail.com>
2022-10-12 11:45:14 -0400

    add q3 market reports

--- js/script-video.js
+++ js/script-video.js
@@ -1,10 +1,10 @@
 // video selector
 
 let videoSource = [
-  'marketing/marketreports/2022/nai_ohio_equities_2022q2_office.mp4',
-  'marketing/marketreports/2022/nai_ohio_equities_2022q2_industrial.mp4',
-  'marketing/marketreports/2022/nai_ohio_equities_2022q2_retail.mp4',
-  // 'marketing/marketreports/2022/nai_ohio_equities_2022q2_multifamily.mp4',
+  'marketing/marketreports/2022/nai_ohio_equities_2022q3_office.mp4',
+  'marketing/marketreports/2022/nai_ohio_equities_2022q3_industrial.mp4',
+  'marketing/marketreports/2022/nai_ohio_equities_2022q3_retail.mp4',
+  // 'marketing/marketreports/2022/nai_ohio_equities_2022q3_multifamily.mp4',
 ];
 
 let videoTag = document.getElementById('videoSourceTag');
