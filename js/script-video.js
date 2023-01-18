2af34f66aff4c5aa612221a9530a743bdd5d2dfa
Andrew <aschnitkey@gmail.com>
2023-01-18 10:50:36 -0500

    update q4 market reports

--- js/script-video.js
+++ js/script-video.js
@@ -1,10 +1,10 @@
 // video selector
 
 let videoSource = [
-  'marketing/marketreports/2022/nai_ohio_equities_2022q3_office.mp4',
-  'marketing/marketreports/2022/nai_ohio_equities_2022q3_industrial.mp4',
-  'marketing/marketreports/2022/nai_ohio_equities_2022q3_retail.mp4',
-  // 'marketing/marketreports/2022/nai_ohio_equities_2022q3_multifamily.mp4',
+  'marketing/marketreports/2022/nai_ohio_equities_2022q4_office.mp4',
+  'marketing/marketreports/2022/nai_ohio_equities_2022q4_industrial.mp4',
+  'marketing/marketreports/2022/nai_ohio_equities_2022q4_retail.mp4',
+  // 'marketing/marketreports/2022/nai_ohio_equities_2022q4_multifamily.mp4',
 ];
 
 let videoTag = document.getElementById('videoSourceTag');
