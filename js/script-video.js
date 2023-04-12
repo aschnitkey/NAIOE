651725cb3485a24bf9823170b7e7cee63b871f33
Andrew <aschnitkey@gmail.com>
2023-04-12 10:17:46 -0400

    add 2023q1 market reports

--- js/script-video.js
+++ js/script-video.js
@@ -1,10 +1,10 @@
 // video selector
 
 let videoSource = [
-  'marketing/marketreports/2022/nai_ohio_equities_2022q4_office.mp4',
-  'marketing/marketreports/2022/nai_ohio_equities_2022q4_industrial.mp4',
-  'marketing/marketreports/2022/nai_ohio_equities_2022q4_retail.mp4',
-  // 'marketing/marketreports/2022/nai_ohio_equities_2022q4_multifamily.mp4',
+  'marketing/marketreports/2022/nai_ohio_equities_2023q1_office.mp4',
+  'marketing/marketreports/2022/nai_ohio_equities_2023q1_industrial.mp4',
+  'marketing/marketreports/2022/nai_ohio_equities_2023q1_retail.mp4',
+  // 'marketing/marketreports/2022/nai_ohio_equities_2023q1_multifamily.mp4',
 ];
 
 let videoTag = document.getElementById('videoSourceTag');
