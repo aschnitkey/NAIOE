197020125de20609a8b4aacb8fffebc0ff55341c
Andrew <aschnitkey@gmail.com>
2024-03-28 15:53:32 -0400

    update clients logo section

--- js/script.js
+++ js/script.js
@@ -19,39 +19,49 @@
 ///////////////////////////////////////////////////////
 
 const allLogos = [
-	{source: "alvis.png", alt: "Alvis logo"},
-	{source: "cameronmitchellrestaurants.png", alt: "Cameron Mitchell Restaurants logo"},
-	{source: "campuspartners.png", alt: "Campus Partners logo"},
-	{source: "covermymeds.png", alt: "Cover My Meds logo"},
-	{source: "duke.png", alt: "Duke Realty logo"},
-	{source: "farmersinsurance.png", alt: "Farmers Insurance logo"},
-	{source: "furniturebank.png", alt: "Furniture Bank logo"},
-	{source: "hackman.png", alt: "Hackman Capital Partners logo"},
-	{source: "kaufman.png", alt: "Kaufman Development logo"},
-	{source: "lox.png", alt: "The Lox logo"},
-	{source: "meritex.png", alt: "Meritex logo"},
-	{source: "mountcarmel.png", alt: "Mount Carmel logo"},
-	{source: "nationwidechildrens.png", alt: "Nationwide Children's Hospital logo"},
-	{source: "officedepot.png", alt: "Office Depot logo"},
-	{source: "ohiohealth.png", alt: "OhioHealth logo"},
-	{source: "ohiostate.png", alt: "The Ohio State University logo"},
-	{source: "pioneer.png", alt: "Pioneer logo"},
-	{source: "pnc.png", alt: "PNC Bank logo"},
-	{source: "scotts.png", alt: "Scotts logo"},
-	{source: "wasserstrom.png", alt: "Wasserstrom logo"},
-	{source: "wendys.png", alt: "Wendy's logo"},
-	{source: "whitehall.png", alt: "City of Whitehall logo"}
+  { source: 'alvis.png', alt: 'Alvis logo' },
+  { source: 'bark.png', alt: 'Bark logo' },
+  {
+    source: 'cameronmitchellrestaurants.png',
+    alt: 'Cameron Mitchell logo',
+  },
+  {
+    source: 'columbusschoolforgirls.png',
+    alt: 'Columbus School For Girls logo',
+  },
+  { source: 'covermymeds.png', alt: 'Cover My Meds logo' },
+  { source: 'daimler.png', alt: 'Daimler Group logo' },
+  { source: 'furniturebank.png', alt: 'Furniture Bank logo' },
+  { source: 'grange.png', alt: 'Grange Insurance logo' },
+  { source: 'hackman.png', alt: 'Hackman Capital Partners logo' },
+  { source: 'healthcarerealtytrust.png', alt: 'Healthcare Realty Trust logo' },
+  { source: 'kaufman.png', alt: 'Kaufman Development logo' },
+  { source: 'lox.png', alt: 'The Lox logo' },
+  { source: 'meritex.png', alt: 'Meritex logo' },
+  { source: 'mountcarmel.png', alt: 'Mount Carmel logo' },
+  {
+    source: 'nationwidechildrens.png',
+    alt: "Nationwide Children's Hospital logo",
+  },
+  { source: 'newalbanycompany.png', alt: 'New Albany Company logo' },
+  { source: 'ohioent.png', alt: 'Ohio ENT logo' },
+  { source: 'ohiohealth.png', alt: 'OhioHealth logo' },
+  { source: 'ohiostate.png', alt: 'The Ohio State University logo' },
+  { source: 'rockbridge.png', alt: 'Rockbridge Capital logo' },
+  { source: 'wasserstrom.png', alt: 'Wasserstrom logo' },
 ];
 let dispLogos = document.querySelectorAll('.logos-list img');
 let logoContainer = [];
 
-while(logoContainer.length < 8) {
-	const randomNum = Math.floor(Math.random() * allLogos.length);
-	if(logoContainer.indexOf(randomNum) === -1) { logoContainer.push(randomNum) }
+while (logoContainer.length < 8) {
+  const randomNum = Math.floor(Math.random() * allLogos.length);
+  if (logoContainer.indexOf(randomNum) === -1) {
+    logoContainer.push(randomNum);
+  }
 }
 
 dispLogos.forEach((logo, i) => {
-	let randomValue = logoContainer[i];
-	logo.setAttribute('src', `img/logos/${allLogos[randomValue].source}`);
-	logo.setAttribute('alt', `${allLogos[randomValue].alt}`);
+  let randomValue = logoContainer[i];
+  logo.setAttribute('src', `img/logos/${allLogos[randomValue].source}`);
+  logo.setAttribute('alt', `${allLogos[randomValue].alt}`);
 });
