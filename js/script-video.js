6e07439d3f216d035963492a29d6b1bf14337041
Andrew <aschnitkey@gmail.com>
2024-04-11 11:11:43 -0400

    2024 q1 market reports

--- js/script-video.js
+++ js/script-video.js
@@ -1,9 +1,9 @@
 // video selector
 
 let videoSource = [
-  'marketing/marketreports/2023/nai_ohio_equities_2023q4_office.mp4',
-  'marketing/marketreports/2023/nai_ohio_equities_2023q4_industrial.mp4',
-  'marketing/marketreports/2023/nai_ohio_equities_2023q4_retail.mp4',
+  'marketing/marketreports/2024/nai_ohio_equities_2024q1_office.mp4',
+  'marketing/marketreports/2024/nai_ohio_equities_2024q1_industrial.mp4',
+  'marketing/marketreports/2024/nai_ohio_equities_2024q1_retail.mp4',
   // 'marketing/marketreports/2023/nai_ohio_equities_2023q3_multifamily.mp4',
 ];
 
