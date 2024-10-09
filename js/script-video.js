b2ba6417f08f60ec7f7e56cdbea7de1f5e5148b6
Andrew <aschnitkey@gmail.com>
2024-10-09 14:39:09 -0400

    add 24q3 market reports

--- js/script-video.js
+++ js/script-video.js
@@ -1,9 +1,10 @@
 // video selector
 
 let videoSource = [
-  'marketing/marketreports/2024/nai_ohio_equities_2024q2_office.mp4',
-  'marketing/marketreports/2024/nai_ohio_equities_2024q2_industrial.mp4',
-  'marketing/marketreports/2024/nai_ohio_equities_2024q2_retail.mp4',
+  'marketing/marketreports/2024/nai_ohio_equities_2024q3_all.mp4',
+  // 'marketing/marketreports/2024/nai_ohio_equities_2024q2_office.mp4',
+  // 'marketing/marketreports/2024/nai_ohio_equities_2024q2_industrial.mp4',
+  // 'marketing/marketreports/2024/nai_ohio_equities_2024q2_retail.mp4',
   // 'marketing/marketreports/2023/nai_ohio_equities_2024q2_multifamily.mp4',
 ];
 
